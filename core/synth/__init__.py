5bb66dc93e2961a338590f0059001462fe85e98d
AdrianSliwa <asliwa1986@gmail.com>
2026-07-08 21:01:26 +0200

    feat(synth): package exports

---
+++ core/synth/__init__.py
@@ -0,0 +1,4 @@
+"""Synthetic data generators. ALL DATA IS SYNTHETIC and clearly labelled."""
+from core.synth import scenario_deler, scenario_konsulent
+
+__all__ = ["scenario_deler", "scenario_konsulent"]
