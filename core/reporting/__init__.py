e2e8b308c990db527b5896cf80fa9240438420ee
AdrianSliwa <asliwa1986@gmail.com>
2026-07-08 20:59:41 +0200

    feat(reporting): package exports

---
+++ core/reporting/__init__.py
@@ -0,0 +1,4 @@
+"""Reporting: classification and (later) report/protokoll generation."""
+from core.reporting.classify import InvoiceCheck, check_invoice
+
+__all__ = ["InvoiceCheck", "check_invoice"]
